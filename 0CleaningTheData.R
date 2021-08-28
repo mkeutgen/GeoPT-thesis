b9bd59de12228e3bc136166314a0827126eda3ff
mkeutgen <66516108+mkeutgen@users.noreply.github.com>
2021-08-28 21:35:19 +0200

    comment script

--- 0CleaningTheData.R
+++ 0CleaningTheData.R
@@ -1,31 +1,32 @@
-S#######################
-## Script Zero ########
-## Cleaning the data ##
-#######################
+### Script Zero v1 ########
+## Cleaning the data #####
+##########################
 
-# 1. Attempt 
+#### 1. Loading libraries ####
 
 library(readxl)
 library(tidyverse)
-library(readxl)
-library(tidyverse)
 
-# Import all datasets
+#### 2. Importing all the late excel files ####
 
-path <- read_excel("GeoPT Raw data late Table 1.xlsx")
+# extract names of the datasets
 sheetnames <- excel_sheets("GeoPT Raw data late Table 1.xlsx")
+# extract the dataframes and gather them in a list 
 mylist <- lapply(excel_sheets("GeoPT Raw data late Table 1.xlsx"),read_excel,path="GeoPT Raw data late Table 1.xlsx")
 # name the dataframes
 names(mylist) <- sheetnames
-# names(mylist) <- substr(names(mylist),1,7)
-names.df <- names(mylist)
-names(list.wf) <- names(mylist)
-# mylist
-list.identifier <- list()
+
+# initialize a series of list :
+# list of the identifiers of the rocks
 list.ID <- list()
+# list of long.format data frames
 list.lf <- list()
+# list of wide.format data frames 
 list.wf <- list()
-# For LOOP of DOOM
+# name the list
+names(list.wf) <- names(mylist)
+
+# Transform the 13 datasets 
 
 for (i in 1:13){
   list.ID[i] <- substring(colnames(mylist[[i]][10]),1,1)
@@ -37,7 +38,6 @@ mylist[[i]] <-  mylist[[i]][-c(1,2),]
 # clean colnames
 colnames(mylist[[i]]) <- make.unique(str_remove(colnames( mylist[[i]]), "\\.+.*"))
 # set the name of the 2nd column to "Unit"
-# names(mylist[[i]])[2]<- "Unit"
 # delete the unit column
 mylist[[i]] <- mylist[[i]][-2]
 
@@ -46,7 +46,7 @@ for (i in 1:13){
 list.lf[[i]] <- mylist[[i]] %>% pivot_longer(col=starts_with(list.ID[[i]]),names_to="Laboratory",values_to="Concentration",names_repair = "universal")
 }
 
-# Lab.Code
+# clean the names of the 'Lab' column, set it to 'Lab.Code'
 names(list.lf[[2]])[1]<- "Lab.Code"
 names(list.lf[[12]])[1]<- "Lab.Code"
 names(list.lf[[13]])[1]<- "Lab.Code"
@@ -57,20 +57,23 @@ for (i in 1:13){
 list.wf[[i]] <- list.lf[[i]] %>% pivot_wider(names_from = Lab.Code, values_from = Concentration)
 }
 
-# Do manually the 3rd dataframe
+#### Transform manually the 3rd dataframe ####
+
 mylist[[3]] <- mylist[[3]][-75,]
 names(mylist[[3]])[1]<- "Lab.Code"
 # transform to long format
 list.lf[[3]] <- mylist[[3]] %>% pivot_longer(col=starts_with(list.ID[[3]]),names_to="Laboratory",values_to="Concentration",names_repair = "universal")
 # transform to wide format
 list.wf[[3]] <- list.lf[[3]] %>% pivot_wider(names_from = Lab.Code, values_from = Concentration)
-#### Ended Here, To resume tomorrow, 28th august 2021
-# write to csv
 
 
+#### Export dataframes to .csv ####
+# set the names of the list.wf to list of df
+names(list.wf) <- names(mylist)
+# export dataframes in .csv
 for(i in 1:length(list.wf) ){
   write.csv(list.wf[[i]],file = paste("/home/max/Documents/MStatistics/Thesis/Repository/Data/",names(list.wf[i]),'.csv',sep=""))
 }
-GeoPT36_78Ra <- read_csv("Data/GeoPT36 -78Ra.csv")
-View(GeoPT36_78Ra)
-print("CONGRATS U BOI") 
+
+
+########################################
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-This is the repository for my master thesis.
+
+# Master Thesis Repository 
+## Description
+This is a GitHub repository for my thesis in statistics at KU Leuven. My thesis named "statistical study of the GeoPT database", aims to investigate the compositional nature of geochemical data.
+
+This repository contains both the data and the Rscripts used to conduct statistical analysis.
+
